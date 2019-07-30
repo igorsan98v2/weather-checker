70850090c205ef5b256ba98fec6206f76b3b58a8
Igor Yutsyk <igorsan98@gmail.com>
2019-07-30 15:40:14 +0300

    added websocket

--- src/main/java/com/ygs/SocketConfig.java
+++ src/main/java/com/ygs/SocketConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.socket.config.annotation.AbstractWebSocketMessage
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 
 @Configuration
@@ -23,8 +26,13 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/gs-guide-websocket");
+     /*   registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*")
+                .withSockJS();;
+     */
 
+        RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+        registry.addEndpoint("/gs-guide-websocket").setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
+                .setAllowedOrigins("*");
     }
 
 }
